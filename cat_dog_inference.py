e3d0c229c5b9fbde45a93b530aa49a62ea1c5ada
peytonhong <hhs8314@gmail.com>
2026-04-25 17:21:07 +0900

    'temporary'

--- cat_dog_inference.py
+++ cat_dog_inference.py
@@ -70,7 +70,7 @@ with torch.no_grad():
         image_path = image_paths[idx]
         image_cv = cv2.imread(image_path)
         image = Image.open(image_paths[idx]).convert('RGB')
-        # temporary comment
+        # temporary comment 
         outputs = model(transform(image).unsqueeze(0).to(device))   # input shape: (C, H, W) --> (1, C, H, W)
         outputs = outputs[0].detach().cpu().numpy()
         label = np.argmax(outputs)
